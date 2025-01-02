98360f949a3b3d41df19a88f1a9c3918f9274bba
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-02 12:01:19 -0400

    Abstract entity creation

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { IAnswersRepository } from "../../repositories/answers-repository";
 import { Answer } from "../entities/answer";
 
@@ -15,10 +16,10 @@ export class AnswerQuestionUseCase {
     questionId,
     content,
   }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId),
     });
 
     await this.answerRepository.create(answer);
