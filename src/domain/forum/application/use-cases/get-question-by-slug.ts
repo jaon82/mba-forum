abce81af9b554f52fd68ed1a4679abf1447e2982
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-03 11:01:24 -0400

    Create "Search by slug" use case

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,24 @@
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import { IQuestionsRepository } from "../repositories/questions-repository";
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string;
+}
+interface GetQuestionBySlugUseCaseResponse {
+  question: Question;
+}
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionsRepository: IQuestionsRepository) {}
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findBySlug(slug);
+    if (!question) {
+      throw new Error("Question not found.");
+    }
+    return {
+      question,
+    };
+  }
+}
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -3,6 +3,15 @@ import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export class InMemoryQuestionsRepository implements IQuestionsRepository {
   public items: Question[] = [];
+
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug);
+    if (!question) {
+      return null;
+    }
+    return question;
+  }
+
   async create(question: Question) {
     this.items.push(question);
   }
