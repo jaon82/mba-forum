90b39a42490208da44799fb49ada5f85ea66d0ba
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-03 16:45:17 -0400

    Add use case for comment on answer

---
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -0,0 +1,40 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { IAnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+import { IAnswersRepository } from "../repositories/answers-repository";
+
+interface CommentOnAnswerUseCaseRequest {
+  authorId: string;
+  answerId: string;
+  content: string;
+}
+interface CommentOnAnswerUseCaseResponse {
+  answerComment: AnswerComment;
+}
+
+export class CommentOnAnswerUseCase {
+  constructor(
+    private answersRepository: IAnswersRepository,
+    private answerCommentsRepository: IAnswerCommentsRepository
+  ) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: CommentOnAnswerUseCaseRequest): Promise<CommentOnAnswerUseCaseResponse> {
+    const answer = await this.answersRepository.findById(answerId);
+    if (!answer) {
+      throw new Error("Answer not found.");
+    }
+    const answerComment = AnswerComment.create({
+      authorId: new UniqueEntityID(authorId),
+      answerId: new UniqueEntityID(answerId),
+      content,
+    });
+    await this.answerCommentsRepository.create(answerComment);
+    return {
+      answerComment,
+    };
+  }
+}
---
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -0,0 +1,11 @@
+import { IAnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comments-repository";
+import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
+
+export class InMemoryAnswerCommentsRepository
+  implements IAnswerCommentsRepository
+{
+  public items: AnswerComment[] = [];
+  async create(answerComment: AnswerComment) {
+    this.items.push(answerComment);
+  }
+}
