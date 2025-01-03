089e0169be5938538fbbc8b14944030bc0d6861e
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-03 10:52:23 -0400

    Refactor unit tests

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,17 +1,23 @@
-import { IAnswersRepository } from "../repositories/answers-repository";
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
 import { AnswerQuestionUseCase } from "./answer-question";
 
-const fakeAnswersRepository: IAnswersRepository = {
-  create: async () => {},
-};
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: AnswerQuestionUseCase;
 
-test("create an answer", async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
-  const answer = await answerQuestion.execute({
-    questionId: "1",
-    instructorId: "1",
-    content: "Nova resposta",
+describe("Create Answer", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new AnswerQuestionUseCase(inMemoryAnswersRepository);
   });
 
-  expect(answer.content).toEqual("Nova resposta");
+  it("should be able to create a answer", async () => {
+    const { answer } = await sut.execute({
+      questionId: "1",
+      instructorId: "1",
+      content: "Conteúdo da resposta",
+    });
+
+    expect(answer.id).toBeTruthy();
+    expect(inMemoryAnswersRepository.items[0].id).toEqual(answer.id);
+  });
 });
