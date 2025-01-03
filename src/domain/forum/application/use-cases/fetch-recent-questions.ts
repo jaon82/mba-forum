0cea545d6138389fd2b1fbd3a15b162a40dd6476
Jean Nunes <jean.nunes@uzzipay.com>
2025-01-03 14:52:06 -0400

    Add List Recent Questions use case

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,22 @@
+import { Question } from "@/domain/forum/enterprise/entities/question";
+import { IQuestionsRepository } from "../repositories/questions-repository";
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number;
+}
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[];
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionsRepository: IQuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionsRepository.findManyRecent({ page });
+    return {
+      questions,
+    };
+  }
+}
